ae335af8b3a300b63b374c6d249bdee09945533f
berkinakaydin <berkinakaydin@gmail.com>
2025-11-27 23:51:04 +0100

    refactor tests

--- src/test/kotlin/com/bol/demo/GameTest.kt
+++ src/test/kotlin/com/bol/demo/GameTest.kt
@@ -1,7 +1,6 @@
 package com.bol.demo
 
 import com.bol.demo.game.Game
-import com.bol.demo.game.Player
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions.assertSoftly
 import org.junit.jupiter.api.BeforeEach
@@ -12,14 +11,10 @@ import kotlin.test.assertNotNull
 
 class GameTest {
     private lateinit var game: Game
-    private lateinit var player1: Player
-    private lateinit var player2: Player
 
     @BeforeEach
     fun setUp() {
         game = Game()
-        player1 = game.player1
-        player2 = game.player2
     }
 
     @Nested
@@ -36,37 +31,37 @@ class GameTest {
         fun `each player should have 6 small pits and one big pit`() {
             val numberOfSmallPits = 6
 
-            assertThat(player1.smallPits.size).isEqualTo(numberOfSmallPits)
-            assertNotNull(player1.largePit)
+            assertThat(game.currentPlayer.smallPits.size).isEqualTo(numberOfSmallPits)
+            assertNotNull(game.currentPlayer.largePit)
         }
 
         @Test
         fun `each small pits should contain 6 stones`() {
             val numberOfStones = 6
 
-            player1.smallPits.forEach { pit ->
+            game.currentPlayer.smallPits.forEach { pit ->
                 assertThat(pit.capacity).isEqualTo(numberOfStones)
             }
-            assertThat(player1.largePit.capacity).isEqualTo(0)
+            assertThat(game.currentPlayer.largePit.capacity).isEqualTo(0)
         }
 
         @Test
         fun `each pits should be connected`() {
             assertSoftly {
-                for (i in 0 until player1.smallPits.size - 1) {
-                    val currentPit = player1.smallPits[i]
-                    val nextPit = player1.smallPits[i + 1]
+                for (i in 0 until game.currentPlayer.smallPits.size - 1) {
+                    val currentPit = game.currentPlayer.smallPits[i]
+                    val nextPit = game.currentPlayer.smallPits[i + 1]
 
                     it.assertThat(currentPit.next).isEqualTo(nextPit)
                 }
-                it.assertThat(player1.largePit).isEqualTo(player1.smallPits.last().next)
+                it.assertThat(game.currentPlayer.largePit).isEqualTo(game.currentPlayer.smallPits.last().next)
             }
         }
 
         @Test
         fun `each players should be connected`() {
-            assertThat(player1.largePit.next).isEqualTo(player2.smallPits.first())
-            assertThat(player2.largePit.next).isEqualTo(player1.smallPits.first())
+            assertThat(game.currentPlayer.largePit.next).isEqualTo(game.opponent.smallPits.first())
+            assertThat(game.opponent.largePit.next).isEqualTo(game.currentPlayer.smallPits.first())
         }
     }
 
@@ -76,14 +71,14 @@ class GameTest {
         @Test
         fun `when a player makes a move from first pit, stones should be sowed`() {
             val chosenPitIndex = 0
-            var currentPit = player1.smallPits[chosenPitIndex]
+            var currentPit = game.currentPlayer.smallPits[chosenPitIndex]
             val numberOfStones = currentPit.capacity
 
-            val oldStateOfPlayer = player1.copy()
+            val oldStateOfPlayer = game.currentPlayer.copy()
             var oldStateOfCurrentPit = oldStateOfPlayer.smallPits[chosenPitIndex]
 
             game.play(chosenPitIndex)
-            assertThat(player1.smallPits[chosenPitIndex].capacity).isEqualTo(0)
+            assertThat(game.currentPlayer.smallPits[chosenPitIndex].capacity).isEqualTo(0)
 
             repeat(numberOfStones) {
                 val newPit = currentPit.next
@@ -97,19 +92,21 @@ class GameTest {
         @Test
         fun `while sowing the stones, stone should not be placed in opponents large pit`() {
             val chosenPitIndex = 0
-            val currentPit = player1.smallPits[chosenPitIndex]
+            val currentPlayer = game.currentPlayer
+            val currentPit = currentPlayer.smallPits[chosenPitIndex]
             currentPit.capacity = 13 //when it is at least 13, that means opponent's pits get stones
 
-            val oldStateOfOpponentPlayer = player2.copy()
+            val opponent = game.opponent
+            val oldStateOfOpponentPlayer = opponent.copy()
 
             game.play(chosenPitIndex)
-            assertThat(player2.largePit.capacity).isEqualTo(oldStateOfOpponentPlayer.largePit.capacity)
+            assertThat(opponent.largePit.capacity).isEqualTo(oldStateOfOpponentPlayer.largePit.capacity)
         }
 
         @Test
         fun `while sowing the stones, current players pits might be changed more than once`() {
             val chosenPitIndex = 0
-            val currentPit = player1.smallPits[chosenPitIndex]
+            val currentPit = game.currentPlayer.smallPits[chosenPitIndex]
             currentPit.capacity = 36 //when it is at least 18, that means opponent's pits get stones
 
             game.play(chosenPitIndex)
@@ -120,43 +117,47 @@ class GameTest {
         @Test
         fun `if players last stone lands not in own big pit, then opponent gets the turn`() {
             val chosenPitIndex = 0
-            val currentPit = player1.smallPits[chosenPitIndex]
+            val currentPlayer = game.currentPlayer
+            val currentPit = currentPlayer.smallPits[chosenPitIndex]
 
             currentPit.capacity = 5
             game.play(chosenPitIndex)
 
-            assertThat(game.currentPlayer).isEqualTo(player2)
+            assertThat(currentPlayer).isEqualTo(game.opponent)
         }
 
         @Test
         fun `if players last stone lands in own big pit, then current player gets another turn`() {
             val chosenPitIndex = 0
-            val currentPit = player1.smallPits[chosenPitIndex]
+            val currentPlayer = game.currentPlayer
+            val currentPit = currentPlayer.smallPits[chosenPitIndex]
 
             currentPit.capacity = 6
             game.play(chosenPitIndex)
 
-            assertThat(game.currentPlayer).isEqualTo(game.currentPlayer)
+            assertThat(currentPlayer).isEqualTo(game.currentPlayer)
         }
 
         @Test
         fun `if players last stone lands in own small pit, player captures stones from the opposite pit`() {
             // given
             val chosenPitIndex = 0
-            val currentPit = player1.smallPits[chosenPitIndex]
+            val currentPlayer = game.currentPlayer
+            val currentPit = currentPlayer.smallPits[chosenPitIndex]
             currentPit.capacity = 1
 
-            val opponentsPit = player2.smallPits[6 - (chosenPitIndex + currentPit.capacity) - 1]
+            val opponent = game.opponent
+            val opponentsPit = opponent.smallPits[6 - (chosenPitIndex + currentPit.capacity) - 1]
             val opponentsPitCapacity = opponentsPit.capacity
 
             // when
-            val emptyPit = player1.smallPits[chosenPitIndex + currentPit.capacity]
+            val emptyPit = currentPlayer.smallPits[chosenPitIndex + currentPit.capacity]
             emptyPit.capacity = 0
 
             game.play(chosenPitIndex)
 
             // then
-            assertThat(game.player1.largePit.capacity).isEqualTo(opponentsPitCapacity + 1)
+            assertThat(currentPlayer.largePit.capacity).isEqualTo(opponentsPitCapacity + 1)
             assertThat(opponentsPit.capacity).isEqualTo(0)
             assertThat(emptyPit.capacity).isEqualTo(0)
         }
@@ -198,19 +199,19 @@ class GameTest {
     fun `end game`() {
         //given
         val chosenPitIndex = 5
-        val currentPit = player1.smallPits[chosenPitIndex]
+        val currentPit = game.currentPlayer.smallPits[chosenPitIndex]
         currentPit.capacity = 2
 
         //when
-        player1.smallPits.filter { it != currentPit }.map { it.capacity = 0 }
-        player2.smallPits.map { it.capacity = 10 }
-        player1.largePit.capacity = 50
+        game.currentPlayer.smallPits.filter { it != currentPit }.map { it.capacity = 0 }
+        game.opponent.smallPits.map { it.capacity = 10 }
+        game.currentPlayer.largePit.capacity = 50
 
         game.play(chosenPitIndex)
 
         //then
-        assertThat(player1.largePit.capacity).isEqualTo(51)
-        assertThat(player2.largePit.capacity).isEqualTo(61)
-        assertThat(game.winner).isNotNull.isEqualTo(player2)
+        assertThat(game.currentPlayer.largePit.capacity).isEqualTo(51)
+        assertThat(game.opponent.largePit.capacity).isEqualTo(61)
+        assertThat(game.winner).isNotNull.isEqualTo(game.opponent)
     }
 }
